5b56944110f46ba285009778f2c362546e911e7f
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-05-05 17:56:09 +0300

    edu class java

---
+++ newEdu/src/Arithmetic.java
@@ -0,0 +1,11 @@
+public class Arithmetic {
+
+    public void summary(int firthNumber, int secondNumber) {
+        int summa = firthNumber + secondNumber;
+        System.out.println(summa);
+    }
+    public void multiplication(int firthNumber, int secondNumber) {
+        int multi = firthNumber * secondNumber;
+        System.out.println(multi);
+    }
+}
--- newEdu/src/Main.java
+++ newEdu/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Arithmetic arithmetic = new Arithmetic();
+
+        int a = 140;
+        int b = 540;
+
+        arithmetic.summary(a, b);
+        arithmetic.multiplication(a, b);
+
     }
 }
