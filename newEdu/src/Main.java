5dce0330d2d556f189c2a38ab209d2c3b4a8c893
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-05-06 12:37:54 +0300

    array

--- newEdu/src/Main.java
+++ newEdu/src/Main.java
@@ -1,14 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Arithmetic arithmetic = new Arithmetic();
-
-        int a = 1;
-        int b = 2;
-
-        arithmetic.summary(a, b);
-        arithmetic.multiplication(a, b);
-        arithmetic.maxOfTwo(a, b);
-        arithmetic.minOfTwo(a, b);
+        System.out.print("Введите количество символов для генерации массива: ");
+        int countArray = new Scanner(System.in).nextInt();
+        System.out.print("Введите количество символов для сдвига вправо: ");
+        int countNumbers = new Scanner(System.in).nextInt();
+        int[] array = new int[countArray];
+        for(int i = 0; i < countArray; i++){
+            array[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(array));
+        for(int i = 0; i < countNumbers; i++){
+            for(int j = 0; j < array.length - 1; j++){
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(array));
 
     }
 }
